6de03f3153773e81de85e4a95b11b5e80de8ddac
Mozadded Ul Islam Noor <pmozadded@gmail.com>
2025-03-11 06:33:40 +0600

    preloader ADDED!

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-mozadded/',
+  base: '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
